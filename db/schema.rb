5becc88be2cb2f5026528872a8b24cf2ae897ea9
rlandows <rlandows@uci.edu>
2017-12-23 12:52:32 -0800

    removed  mailer issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223195355) do
+ActiveRecord::Schema.define(version: 20171220041404) do
 
   create_table "budgetevents", force: :cascade do |t|
     t.string   "title"
@@ -110,13 +110,8 @@ ActiveRecord::Schema.define(version: 20171223195355) do
     t.integer  "role"
     t.string   "name"
     t.boolean  "display",                default: true
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
