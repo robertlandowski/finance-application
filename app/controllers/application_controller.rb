6a0fe6469f9b8994bcd41bbd9cdfb7c83afccff3
rlandows <rlandows@uci.edu>
2017-12-23 21:09:17 -0800

    timezone edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # before_action :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :timezone
+  end
+
   private
    def require_sign_in
      unless current_user
